ce7e89fe680d48fbfbee1c2151f62e3fe2601163
Anderson Galicia <Anderssnjavrmz1@outlook.es>
2025-03-24 00:56:42 -0600

    Se agrego el controlador y las vistas y tambien se crearon los registros

---
+++ AJGR20250324S2.AppWebMVC/Models/EscuelaContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AJGR20250324S2.AppWebMVC.Models;
+
+public partial class EscuelaContext : DbContext
+{
+    public EscuelaContext()
+    {
+    }
+
+    public EscuelaContext(DbContextOptions<EscuelaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Estudiante> Estudiantes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=Andrrssn\\SQLEXPRESS;Initial Catalog=Escuela;Integrated Security=True;Encrypt=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Estudiante>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Estudian__3214EC270C536BC9");
+
+            entity.ToTable("Estudiante");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.CorreoElectronico)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.FechaInscripcion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Promedio).HasColumnType("decimal(4, 2)");
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ AJGR20250324S2.AppWebMVC/Models/Estudiante.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AJGR20250324S2.AppWebMVC.Models;
+
+public partial class Estudiante
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Los nombres son obligatorios")]
+    [Display(Name = "Nombres")]
+    public string Nombre { get; set; } = null!;
+
+    [Required(ErrorMessage = "Los apellidos son obligatorios")]
+    [Display(Name = "Apellidos")]
+    public string Apellido { get; set; } = null!;
+
+    [Required(ErrorMessage = "La fecha de nacimiento es obligatoria")]
+    [Display(Name = "Fecha de Nacimiento")]
+    public DateOnly FechaNacimiento { get; set; }
+
+    [Display(Name = "Correo Electrónico")]
+    public string? CorreoElectronico { get; set; }
+
+    [Display(Name = "Teléfono")]
+    public string? Telefono { get; set; }
+
+    [Required(ErrorMessage = "El grado es obligatorio")]
+    public int Grado { get; set; }
+
+    public decimal? Promedio { get; set; }
+
+    [Required(ErrorMessage = "La fecha de inscripción es obligatoria")]
+    [Display(Name = "Fecha de Inscripción")]
+    public DateTime? FechaInscripcion { get; set; }
+}
