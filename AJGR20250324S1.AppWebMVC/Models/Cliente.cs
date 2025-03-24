df3fc90f6a217e2b1ca7a5f7a81fe89a1f1c777d
Anderson Galicia <Anderssnjavrmz1@outlook.es>
2025-03-23 21:44:22 -0600

    Se alizo todo lo que requeria el CRUD

---
+++ AJGR20250324S1.AppWebMVC/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AJGR20250324S1.AppWebMVC.Models;
+
+public partial class Cliente
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre del cliente es obligatorio.")]
+    public string Nombre { get; set; } = null!;
+
+    [Required(ErrorMessage = "La dirección del cliente es obligatoria.")]
+    [Display(Name = "Dirección")]
+    public string Direccion { get; set; } = null!;
+
+    [Required(ErrorMessage = "El teléfono del cliente es obligatoria.")]
+    [Display(Name = "Teléfono")]
+    public string Telefono { get; set; } = null!;
+
+    public string? Email { get; set; }
+}
